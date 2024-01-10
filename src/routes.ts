531f30107b324351b162a154ea77ecf6aac9f81a
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-01-10 18:58:55 -0300

    Acionando Product, controller e services e na rota.

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,8 @@ import { DetailUserController } from './controllers/user/DetailUserController'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 
+import { CreateProductController } from './controllers/product/CreateProductController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 
@@ -23,5 +25,9 @@ router.get('/me', isAuthenticated, new DetailUserController().handle )
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+// -- ROTAS PRODUCT --
+router.post('/product', isAuthenticated, new CreateProductController().handle )
+
 
 export { router }; 
